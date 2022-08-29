1dd8fbc140d53e0de141b0306aba3f3e1238c61a
saia-maka <makasiosaia@gmail.com>
2022-08-29 20:28:50 +1200

    fix exhibit component

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Banner />
-      <Exhibit></Exhibit>
+      <Exhibit />
     </>
   )
 }
--- sextant/src/Exhibit.jsx
+++ sextant/src/Exhibit.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from 'react'
 
 const ExhibitTitle = () => {
-  return <div className="Exhibit-Title-Wrapper">
-    <h2 className='Exhibit-Title'>Sextant Exhibit</h2>
-  </div>
+  return (
+    <div className="Exhibit-Title-Wrapper">
+      <h2 className="Exhibit-Title">Sextant Exhibit</h2>
+    </div>
+  )
 }
 
-export const Exhibit = (children) => {
-  return <div className='Exhibit-Wrapper'>
-    <ExhibitTitle />
-    {children}
-  </div>
+export const Exhibit = ({ children }) => {
+  return (
+    <div className="Exhibit-Wrapper">
+      <ExhibitTitle />
+      {children}
+    </div>
+  )
 }
