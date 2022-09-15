9492bee3f57575b9003fbdc9260d2547b1b8d406
saia-maka <makasiosaia@gmail.com>
2022-09-15 21:55:40 +1200

    tidy banner component

--- sextant/src/Banner.jsx
+++ sextant/src/Banner.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 export const Banner = () => {
-  return <div className='Banner-Wrapper'>
-    <h1 className='Banner-Title'>Welcome to my <u className='Banner-Title-UL'> Sextant </u> App</h1>
-  </div>
+  return (
+    <div className="Banner-Wrapper">
+      <h1 className="Banner-Title">
+        Welcome to my <u className="Banner-Title-UL"> Sextant </u> App
+      </h1>
+    </div>
+  )
 }
-
