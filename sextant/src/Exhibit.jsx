c15bdf779254deeb154ef3b59804a95c112d13e8
saia-maka <makasiosaia@gmail.com>
2022-09-15 21:54:43 +1200

    render API data

--- sextant/src/Exhibit.jsx
+++ sextant/src/Exhibit.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { API } from './API'
 
 const ExhibitTitle = () => {
   return (
@@ -8,11 +9,49 @@ const ExhibitTitle = () => {
   )
 }
 
-export const Exhibit = ({ children }) => {
+const ExhibitItem = ({ ipAddress }) => {
+  const ItemTitle = () => {
+    return ipAddress.length > 14 ? (
+      <h2>Your IP (IPv6) Address is:</h2>
+    ) : (
+      <h2> Your IP (IPv4) Address is:</h2>
+    )
+  }
+  return (
+    <div className="Exhibit-Card">
+      <ItemTitle />
+      <p>{ipAddress}</p>
+    </div>
+  )
+}
+
+export const Exhibit = () => {
+  const [ipv4, setIpv4] = useState([])
+  const [ipv6, setIpv6] = useState([])
+  useEffect(() => {
+    API('https://api.ipify.org?format=json')
+      .then((res) => {
+        setIpv4(res)
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  }, [])
+  useEffect(() => {
+    API('https://api64.ipify.org?format=json')
+      .then((res) => {
+        setIpv6(res)
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  }, [])
+
   return (
     <div className="Exhibit-Wrapper">
       <ExhibitTitle />
-      {children}
+      <ExhibitItem ipAddress={ipv4} />
+      <ExhibitItem ipAddress={ipv6} />
     </div>
   )
 }
